bd982ab87d69522d714f0130b34061412ad624bf
Mathijs Kemp <mathijs.kemp@vennster.nl>
2023-07-31 15:38:53 +0200

    Included wrong main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import reportAccessibility from './lib/reportAccessibility';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { setNonce } from "@ionic/core/components";
 
-const container = document.getElementById('root');
+setNonce("**CSP_NONCE**");
+
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-reportAccessibility(React);
